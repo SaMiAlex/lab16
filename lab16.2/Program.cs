9c8722de082e05c06234ead1bf690053f092233f
SaMiAlex <samias@mail.ru>
2021-10-23 16:45:38 +0600

    Добавьте файлы проекта.

---
+++ lab16.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace lab16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Products.json";
+            string jsonString = "";
+            double maxprice = 0;
+            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            
+            Goods good = JsonSerializer.Deserialize<Goods>(jsonString); //не могу понять как из json файла достать полноценный массив, с которым можно потом работать
+
+            /*for (int i = 0; i < good.Length; i++)
+            {
+                if (maxprice < good.price)
+                {
+                    maxprice = good.price;
+                }
+            }*/
+            Console.WriteLine(maxprice);
+            Console.ReadKey();
+        }
+    }
+    class Goods
+    {
+        [JsonPropertyName("Артикул")]
+        public int Code { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string Name { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+        public Goods(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ lab16/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace lab16
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "Products.json";
+            Console.WriteLine("Введите количество товаров");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Goods[] goodsarray = new Goods [n];
+            if (!File.Exists(path))//создаем файл
+            {
+                File.Create(path);
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            for (int i = 0; i < n; i++)//наполняем массив
+            {
+                Goods good = new Goods();
+                goodsarray[i] = good;
+            }
+            using (StreamWriter sw = new StreamWriter(path))//пишем массив в файл
+            {
+                string jsonString = JsonSerializer.Serialize(goodsarray, options);
+                sw.WriteLine(jsonString);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Goods
+    {
+        [JsonPropertyName("Артикул")]
+        public int Code { get; set; }
+        [JsonPropertyName("Название товара")]
+        public string Name { get; set; }
+        [JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+        public Goods()
+        {
+            try
+            {
+                Console.WriteLine("Введите код товара");
+                Code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара");
+                Name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара");
+                Price = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
+
+
+
+
